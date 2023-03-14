5e00a5d9a6eba67eafe495468e423dafdea56f1b
Manasa <manasagowda787@gmail.com>
2023-03-14 15:22:45 +0530

    shell script

---
+++ firstsrcipt.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#echo Hello manasa
+#variables
+#uppercase by convention
+#letters, numbers, underscores
+#NAME="Manasa"
+#echo "My name is $NAME"
+#echo $NAME
+#User input
+read -p "enter your name: " Name
+echo "Hello $Name, nice to meet you"
